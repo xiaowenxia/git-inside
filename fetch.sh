750542d2035afcdcdae51ba45e6b8f0b14c56e74
xiaowenxia <775117471@qq.com>
2021-02-16 07:28:25 +0000

    disable travis

--- fetch.sh
+++ fetch.sh
@@ -5,5 +5,6 @@ for file in ../git-*.md; do
     contentName=`awk 'NR==1{print}' ${file} | awk -F'## ' '{print $2}'`
     echo 获取文档名称：$contentName
     # 插入 hexo 规范的文档结构
-    cat $file | sed -e "1i\---\ntitle: ${contentName}\n---\n" > ./source/_posts/`basename ${file}`
+    baseName=`basename ${file}`
+    cat $file | sed -e "1i\---\ntitle: ${contentName}\n---\n" > ./source/_posts/${baseName}
 done
