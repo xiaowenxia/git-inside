69b7905b1e795c5ef4895a76e6cef6fb23839c15
xiaowenxia <775117471@qq.com>
2021-02-16 07:11:45 +0000

    Update fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -2,9 +2,8 @@
 for file in ../git-*.md; do
     echo $file
     # 生成文档名称
-    contentName=` awk 'NR==1{print}' ${contentFile} | awk -F'## ' '{print $2}'`
+    contentName=`awk 'NR==1{print}' ${file} | awk -F'## ' '{print $2}'`
     echo 获取文档名称：$contentName
     # 插入 hexo 规范的文档结构
-    sed -i '1i\${contentName}\n' $file
-    cp $file ./source/_posts/
+    cat $file | sed -e "1i\---\ntitle: ${contentName}\n---\n" > ./source/_posts/`basename ${file}`
 done
