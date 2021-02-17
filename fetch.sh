81652b147c07560f32f54c883e6f87c2a14d3a7e
xiaowenxia <775117471@qq.com>
2021-02-17 02:54:39 +0000

    update

--- fetch.sh
+++ fetch.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-for file in ../git-*.md; do
+for file in ./git-*.md; do
     echo $file
     # 生成文档名称
     contentName=`awk 'NR==1{print}' ${file} | awk -F'## ' '{print $2}'`
-    echo 获取文档名称：$contentName
+    contentDate=`git log --all --pretty=format:"%ai" -1 -- $file`
+    echo 获取文档名称：$contentName 日期：$contentDate
     # 插入 hexo 规范的文档结构
     baseName=`basename ${file}`
-    cat $file | sed -e "1i\---\ntitle: ${contentName}\n---\n" > ./source/_posts/${baseName}
+    cat $file | sed -e "1i\---\ntitle: ${contentName}\ndate: ${contentDate}\n---\n" > ./git-inside.2/source/_posts/${baseName}
 done
