a822cbe122d9a05cca5becf06c654ec134ce4e57
xiaowenxia <775117471@qq.com>
2021-02-17 02:58:56 +0000

    update

--- fetch.sh
+++ fetch.sh
@@ -7,5 +7,5 @@ for file in ./git-*.md; do
     echo 获取文档名称：$contentName 日期：$contentDate
     # 插入 hexo 规范的文档结构
     baseName=`basename ${file}`
-    cat $file | sed -e "1i\---\ntitle: ${contentName}\ndate: ${contentDate}\n---\n" > ./git-inside.2/source/_posts/${baseName}
+    cat $file | sed -e "1i\---\ntitle: ${contentName}\ndate: ${contentDate}\n---\n" > ./git-inside/source/_posts/${baseName}
 done
